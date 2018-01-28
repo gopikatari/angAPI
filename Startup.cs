75bb786348e484e3277d3ca56d417293814e731a
Gopi Katari <katarigopi@gmail.com>
2018-01-28 14:38:40 +0530

    commt cors

--- Startup.cs
+++ Startup.cs
@@ -35,6 +35,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +45,7 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseCors(a=>a.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
         }
     }
